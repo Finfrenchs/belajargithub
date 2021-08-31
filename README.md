f596e7cfda200972c2cea05939ba917d1121b256
M. Kelvin Madrianto F <88362497+Finfrenchs@users.noreply.github.com>
2021-08-31 16:58:23 +0700

    Development (#1)
    * First Commit "add README.md"
    
    * Second Commit "Edit README.md and Add Hello World"
    
    * Third Commit "Add Button On click"

---
+++ README.md
@@ -0,0 +1,6 @@
+# Belajar Github
+==
+Repositori ini berisi riwayat belajar github di dicoding
+--
+isi repository
+- file Hello World
--- app/src/main/java/com/kelvin/project/belajargithub/MainActivity.kt
+++ app/src/main/java/com/kelvin/project/belajargithub/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kelvin.project.belajargithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initials Button
+        val button = findViewById<Button>(R.id.btn1)
+        //Onclick Button
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "I'm Coming in the world", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
